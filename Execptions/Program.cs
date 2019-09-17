34836bbe9d0957613c28e5a50920489d1934b253
Krzysiek Wróblewski <krzysiuw1906@hotmail.com>
2019-09-17 17:40:43 +0200

    Add project files.

---
+++ Execptions/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Execptions
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //try
+            //{
+            //    int a, b;
+            //    a = 2;
+            //    b = 0;
+            //Test test = new T
+
+            //Test.Name;
+            var name = ConfigurationManager.AppSettings["name"];
+            Console.WriteLine(name);
+                //Names names = Names.Krzysiek;
+                //switch (names)
+                //{
+                //    case Names.Roman:
+                //        break;
+                //    case Names.Anna:
+                //        break;
+                //    case Names.Krzysiek:
+                //        Console.WriteLine(Names.Krzysiek.ToString());
+                //        Console.WriteLine((int)Names.Krzysiek);
+                //        break;
+                //    default:
+                //        break;
+                //}
+            //    throw new Exception("testowy blad");
+            //    int x = a / b;
+            //}
+            //catch (DivideByZeroException zero)
+            //{
+            //    Console.WriteLine(zero.Message);
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message); 
+            //}
+            //finally
+            //{
+            //    Console.WriteLine("koniec");
+            //}
+            Console.Read();
+        }
+    }
+}
